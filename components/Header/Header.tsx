dc7873d7a17066fcc71c2cb3ab88ca0f69111544
karollann <lannkarol@gmail.com>
2023-03-01 21:32:03 +0100

    add header, font and BookButton component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,11 @@
+import { BookButton } from "../BookButton/BookButton";
+
 import styles from "./header.module.scss";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <p className={styles.header__title}>Homestead Lodge</p>
+      <p className={styles.header__title}>Getaway Lodge</p>
       <nav className={styles.header__nav}>
         <a href="#" className={styles.header__link}>
           About
@@ -18,8 +20,12 @@ export const Header = () => {
           Reviews
         </a>
         <a href="#" className={styles.header__link}>
+          Location
+        </a>
+        <a href="#" className={styles.header__link}>
           Contact
         </a>
+        <BookButton buttonText="Book Now" />
       </nav>
     </header>
   );
